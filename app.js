359dfa349c97bbdffd9ee299a22f9502dc11e2ef
Alexander Hernández <ja.hernandez211@gmail.com>
2023-07-13 18:24:40 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+// pido un número por medio de un prompt
+const primerNumero = parseFloat(prompt("Digite un número: "));
+
+//tomo el número del usuario y lo entre cinco
+const resultMultiplo = primerNumero/5;
+//Convierto el resultado en número entero
+let resultEntero = Math.floor(resultMultiplo);
+
+console.log(resultEntero);
+
+document.getElementById('numero').innerHTML = primerNumero; 
+
+//creo una array vacia para guardar los multiplos de cinco
+multiplos=[];
+n = 1; // mi conatdor
+
+
+while ( n <= resultEntero){
+    console.log(n)
+    let newResult = n*5;
+    console.log(newResult);
+    multiplos.push(newResult);
+    n ++;
+    
+}
+console.log(multiplos);  
+
+var foo = multiplos.map(function(i){
+    return  i + "";
+  })
+  document.getElementById("foo").innerHTML = foo;
